ff99be9e396c26c54bde83d3f85bff8954bf790d
nndeto <86444679+nndeto@users.noreply.github.com>
2024-02-06 19:56:24 -0800

    created variables and ids and classes between HTML and JS

---
+++ app/app.js
@@ -0,0 +1,11 @@
+// get elements we want to manipulate
+let button = document.getElementById("submit")
+let dayInput = document.getElementById("day")
+let dayError = document.getElementById("day-error")
+let monthInput = document.getElementById("month")
+let monthError = document.getElementById("month-error")
+let yearInput = document.getElementById("year")
+let yearError = document.getElementById("year-error")
+let yearText = document.getElementsByClassName("year-text")
+let monthText = document.getElementsByClassName("month-text")
+let dayText = document.getElementsByClassName("day-text")
